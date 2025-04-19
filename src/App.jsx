ecfe6fa79e36bcc6c518e64ffd837876abbb0856
Florian <florian@deleau.org>
2025-04-19 10:20:59 +0200

    Fix bug formulaire v2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    fetch('https://jobs-etudiants-backend.onrender.com/api/jobs')
+    fetch('https://jobs-etudiants-backend.onrender.com/annonces')
       .then(res => res.json())
       .then(data => setJobs(data))
       .catch(err => console.error('Error while fetching jobs:', err));
--- src/components/AddJobForm.jsx
+++ src/components/AddJobForm.jsx
@@ -12,7 +12,7 @@ function AddJobForm({ onAdd }) {
     e.preventDefault();
     const newJob = { title, location, hours, description };
 
-    fetch('https://jobs-etudiants-backend.onrender.com/api/jobs', {
+    fetch('https://jobs-etudiants-backend.onrender.com/annonces', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(newJob)
