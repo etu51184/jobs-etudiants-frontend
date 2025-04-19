26839e55889cdaf7831d8173d123c95101c2c402
Florian <florian@deleau.org>
2025-04-19 10:46:04 +0200

    Login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import AddJobForm from './components/AddJobForm';
 import Job from './components/Job';
+import AuthModal from './components/AuthModal';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [showAuth, setShowAuth] = useState(false);
 
   useEffect(() => {
     fetch('https://jobs-etudiants-backend.onrender.com/api/jobs')
@@ -15,7 +17,12 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Student Jobs in Namur</h1>
+      {/* Barre du haut */}
+      <div className="top-bar">
+        <h1>Student Jobs in Namur</h1>
+        <button className="auth-button" onClick={() => setShowAuth(true)}>Login / Sign up</button>
+      </div>
+
       <p>Welcome to our job platform!</p>
 
       <AddJobForm onAdd={(job) => setJobs([...jobs, job])} />
@@ -27,6 +34,9 @@ function App() {
           <Job key={index} data={job} />
         ))
       )}
+
+      {/* Popup d'authentification */}
+      {showAuth && <AuthModal onClose={() => setShowAuth(false)} />}
     </div>
   );
 }
