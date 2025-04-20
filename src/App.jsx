6203408f7b574e5f26f2d0ade08eb19366faae6e
Florian <florian@deleau.org>
2025-04-20 15:46:12 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import AuthPage from './pages/AuthPage';
-import PostJobPage from './pages/PostJobPage';
-import JobDetails from './pages/JobDetails';
-import AdminUsersPage from './pages/AdminUsersPage';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
-
-function AppRoutes() {
-  const { username } = useAuth();
-
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="auth" element={<AuthPage />} />
-        <Route path="post" element={<PostJobPage />} />
-        <Route path="job/:id" element={<JobDetails />} />
-        <Route
-          path="admin/users"
-          element={username === "admin" ? <AdminUsersPage /> : <Navigate to="/" />}
-        />
-      </Route>
-    </Routes>
-  );
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AuthPage from "./pages/AuthPage";
+import AddJobForm from "./pages/AddJobForm";
+import AdminUsersPage from "./pages/AdminUsersPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <AppRoutes />
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/add-job" element={<AddJobForm />} />
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
