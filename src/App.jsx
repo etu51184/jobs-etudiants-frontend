eb6899c485b8ec94a1195fa269588817c19fcf40
Florian <florian@deleau.org>
2025-04-20 17:13:01 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
 import AddJobForm from './components/AddJobForm/AddJobForm';
 import AdminUsersPage from "./pages/AdminUsersPage";
+import JobDetails from "./pages/JobDetails";
 import Layout from "./components/Layout";
 import './App.css';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}> 
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<AuthPage />} />
         <Route path="/add-job" element={<AddJobForm />} />
         <Route path="/admin/users" element={<AdminUsersPage />} />
-      </Routes>
-    </Layout>
+        <Route path="/job/:id" element={<JobDetails />} />
+      </Route>
+    </Routes>
   );
 }
 
